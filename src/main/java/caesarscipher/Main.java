aec6b6875f72ebf2cc8da5058032155391a63a37
Daniel Ferreira <dl.ferreira179@gmail.com>
2020-02-28 23:21:20 -0300

    Initial commit

---
+++ src/main/java/caesarscipher/Main.java
@@ -0,0 +1,16 @@
+package caesarscipher;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String message = "how are you doing today";
+        int offset = 12;
+
+        CaesarsCipher caesarsCipher = new CaesarsCipher();
+        String cipheredMessage = caesarsCipher.cipher(message, offset);
+
+        System.out.println("Message: " + message);
+        System.out.println("Offset: " + offset);
+        System.out.println("Ciphered message: " + caesarsCipher.cipher(message, offset));
+    }
+}
---
+++ src/test/java/caesarscipher/CaesarsCipherTest.java
@@ -0,0 +1,20 @@
+package caesarscipher;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaesarsCipherTest {
+
+    private CaesarsCipher caesarsCipher = new CaesarsCipher();
+
+    @Test
+    void testCipheredMessageWithOffsetOf12() {
+        assertEquals("tai mdq kag pauzs fapmk", caesarsCipher.cipher("how are you doing today", 12));
+    }
+
+    @Test
+    void testEmptyString() {
+        assertEquals("", caesarsCipher.cipher("", 12));
+    }
+}
